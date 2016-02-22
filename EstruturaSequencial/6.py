95e8b61b31c8b1250a7d5a8e20db1fe0fa9dca97
Lucas Pugliesi <me@fplucas.com>
2016-02-22 17:01:49 -0300

    Estrutura Sequencial - Exercício 6

---
+++ EstruturaSequencial/6.py
@@ -0,0 +1,6 @@
+from math import pi
+print('Vamos calcular a área de um círculo')
+print('Informe o raio do círculo')
+raio = float(input())
+area = pi * (raio ** 2)
+print('A área do círculo é %s' % area)
