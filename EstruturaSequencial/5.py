31529af5345161abc0dc368ce57183c64487d00a
Lucas Pugliesi <me@fplucas.com>
2016-02-22 16:33:27 -0300

    Estrutura Sequencial - Exercício 5

---
+++ EstruturaSequencial/5.py
@@ -0,0 +1,5 @@
+print('Vamos converter metros para centímetros!')
+print('Entre com um número em metros')
+metros = int(input())
+centimetros = metros * 100
+print('%s metros equivale a %s centímetros' % (metros, centimetros))
