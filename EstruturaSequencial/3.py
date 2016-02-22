574f54f51cdb91d1fb7ab21a1dcc79d4d4aecd36
Lucas Pugliesi <me@fplucas.com>
2016-02-22 16:11:21 -0300

    Estrutura Sequencial - Exercício 3

---
+++ EstruturaSequencial/3.py
@@ -0,0 +1,7 @@
+print('Vamos somar!')
+print('Informe o primeiro número')
+numero1 = int(input())
+print('Informe o segundo número')
+numero2 = int(input())
+soma = numero1 + numero2
+print('A soma de %s com %s é igual a %s' % (numero1, numero2, soma))
