55dd1044b3e0b9dc181cdd05e94275f1c5e8433c
Lucas Pugliesi <me@fplucas.com>
2016-02-22 17:14:42 -0300

    Estrutura Sequencial - Exercício 7

---
+++ EstruturaSequencial/7.py
@@ -0,0 +1,7 @@
+print('Vamos calcular a área de um quadrado')
+print('Informe o tamanho da aresta')
+aresta = float(input())
+area = aresta ** 2
+print('A área do quadrado é %s' % area)
+dobro_da_area = area * 2
+print('O dobro da área é %s' % dobro_da_area)
