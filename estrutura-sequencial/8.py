2053bd6ff637b37921feed15f003ace66366838f
Lucas Pugliesi <me@fplucas.com>
2016-02-23 09:21:33 -0300

    Estrutura Sequencial - Exercício 8

---
+++ estrutura-sequencial/8.py
@@ -0,0 +1,7 @@
+print('Vamos calcular seu salário!')
+print('Quanto você ganha por hora?')
+salario_por_hora = float(input())
+print('Quantas horas você trabalha por mês?')
+horas_trabalhadas = float(input())
+salario = salario_por_hora * horas_trabalhadas
+print('Seu salário mensal é %s' % salario)
