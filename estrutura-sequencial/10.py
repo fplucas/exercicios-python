c84dadd09ff72a890a77cbc661d139beaa72ff04
Lucas Pugliesi <me@fplucas.com>
2016-02-25 08:20:09 -0300

    Estrutura Sequencial - Exercício 10

---
+++ estrutura-sequencial/10.py
@@ -0,0 +1,4 @@
+print('Entre com a temperatura em Celsius')
+temperatura_c = int(input())
+temperatura_f = (9 * temperatura_c / 5) + 32
+print('%so. Celsius equivale a %so. Farenheit' % (temperatura_c, temperatura_f))
