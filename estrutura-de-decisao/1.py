c39b10e8ae0ff87d64a10aa15be1b7dc0ae2b17e
Lucas Pugliesi <me@fplucas.com>
2016-03-16 18:12:12 -0300

    Estrutura de decisão - Exercício 1

---
+++ estrutura-de-decisao/1.py
@@ -0,0 +1,8 @@
+print('Entre com um número')
+numero1 = int(input())
+print('Entre com outro número')
+numero2 = int(input())
+if numero1 > numero2:
+    print('O maior número é o %s' % numero1)
+else:
+        print('O maior número é o %s' % numero2)
