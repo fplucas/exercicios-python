62bc7d62f5f4077644c5688c73662652477bd220
Lucas Pugliesi <me@fplucas.com>
2016-03-18 15:40:24 -0300

    Estrutura de decisão - Exercício 3

---
+++ estrutura-de-decisao/3.py
@@ -0,0 +1,8 @@
+print('Entre com o seu sexo (F - Feminino, M - Masculino)')
+sexo = input().upper()
+if sexo == 'F':
+    print('F - Feminino')
+elif sexo == 'M':
+    print('M - Masculino')
+else:
+    print('Sexo inválido')
