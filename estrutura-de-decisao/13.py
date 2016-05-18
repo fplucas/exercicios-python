cd2a3f4894f1121eb159dac7125b58e04430b006
Lucas Pugliesi <me@fplucas.com>
2016-05-18 12:46:32 -0300

    Estrutura de decisão - Exercício 13

---
+++ estrutura-de-decisao/13.py
@@ -0,0 +1,18 @@
+numero = int(input('Entre com um número de 1 a 7: '))
+
+if numero == 1:
+    print('1 - Domingo')
+elif numero == 2:
+    print('2 - Segunda')
+elif numero == 3:
+    print('3 - Terça')
+elif numero == 4:
+    print('4 - Quarta')
+elif numero == 5:
+    print('5 - Quinta')
+elif numero == 6:
+    print('6 - Sexta')
+elif numero == 7:
+    print('7 - Sábado')
+else:
+    print('Valor inválido')
