6c459085bf9973b007d1358da592b8cff3d7bfd0
Lucas Pugliesi <me@fplucas.com>
2016-06-01 11:43:28 -0300

    Estrutura de repetição - Exercício 36

--- estrutura-de-repeticao/36.py
+++ estrutura-de-repeticao/36.py
@@ -4,6 +4,12 @@ def tabuada(numero, inicio, fim):
         print('{} X {} = {}'.format(numero, x, resultado))
 
 numero = int(input('Montar a tabuada de: '))
-inicio = int(input('Começar por: '))
-fim = int(input('Terminar em: '))
+intervalo_invalido = True
+while(intervalo_invalido):
+    inicio = int(input('Começar por: '))
+    fim = int(input('Terminar em: '))
+    if(inicio >= fim):
+        print('Intervalo inválido!')
+    else:
+        intervalo_invalido = False
 tabuada(numero, inicio, fim)
