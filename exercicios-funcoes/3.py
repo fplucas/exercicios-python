5994528cc9de34b452beb31edd7ff4275cd08295
Lucas Pugliesi <me@fplucas.com>
2016-06-15 17:09:15 -0300

    Exercícios com funções - Exercício 3

---
+++ exercicios-funcoes/3.py
@@ -0,0 +1,9 @@
+def soma(a, b, c):
+    resultado = a + b + c
+    return resultado
+
+a = int(input('1o. Número: '))
+b = int(input('2o. Número: '))
+c = int(input('3o. Número: '))
+resultado = soma(a, b, c)
+print(resultado)
