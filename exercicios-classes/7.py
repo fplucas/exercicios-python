0785818cc41315de8a2e392487af5fffefc0847e
Lucas Pugliesi <me@fplucas.com>
2016-06-27 11:15:00 -0300

    Exercícios com classes - Exercício 7

---
+++ exercicios-classes/7.py
@@ -0,0 +1,33 @@
+class Bichinho_virtual():
+    def __init__(self, nome, fome, saude, idade):
+        self._nome = nome
+        self._fome = fome
+        self._saude = saude
+        self._idade = idade
+
+    def alterar_nome(self, nome):
+        self._nome = nome
+
+    def alterar_fome(self, fome):
+        self._fome = fome
+
+    def alterar_saude(self, saude):
+        self._saude = saude
+
+    def alterar_idade(self, idade):
+        self._idade = idade
+
+    def retornar_nome(self):
+        return self._nome
+
+    def retornar_fome(self):
+        return self._fome
+
+    def retornar_saude(self):
+        return self._saude
+
+    def retornar_idade(self):
+        return self._idade
+
+    def retornar_humor(self):
+        return (self._fome + self._saude) / 2
